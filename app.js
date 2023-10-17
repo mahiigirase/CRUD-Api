97044266bb5bbaf1c82908745300e4c7af26cd28
mahiigirase <140257127+mahiigirase@users.noreply.github.com>
2023-10-17 17:13:02 +0530

    change in app file

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const userRoute = require('./Route/userRoute');
 const movieRoute = require('./Route/movieRoute');
 const productRoute = require('./Route/productRoute')
 require('dotenv/config');
+const cors = require('cors')
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 // Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-  res.header('Access-Control-Allow-Origin', 'https://crud-api-service.onrender.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+//   res.header('Access-Control-Allow-Origin', 'https://crud-api-service.onrender.com');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 //default api
 app.get('/', (req,res)=>{
     res.send("this is home")
